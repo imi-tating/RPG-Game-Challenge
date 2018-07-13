7bc94c3c31819f19e694728174b6eab87634e73c
Mandi Blanton <imitating@yahoo.com>
2018-07-12 22:21:36 -0700

    js file updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,10 @@
+var mainCharacterActive = false;
+var mainFrenemieActive = false;
+var mainCharacterHP
+var mainCharacterAP
+var mainFrenemieHP
+var mainFrenemieCAP
+
 function setCharacterPowers () {
   $(".progress-bar").attr("aria-valuenow", "100").css("width", "100%");
   $("#character-ellie-cat").attr({"data-health-points":"140", "data-attack-power":"19", "data-counter-attack-power":"12"});
@@ -10,9 +17,42 @@ function setCharacterPowers () {
   $("#red-laser-hp-text").text("200");
 }
 
-$(document).ready(function () {
+function attackSequence () {
+  mainFrenemieHP -= mainCharacterAP;
+  mainCharacterAP += mainCharacterAP;
+  mainCharacterHP -= $
+}
 
+$(document).ready(function () {
   setCharacterPowers();
+
+  $(".character").on("click", function(){
+    if (mainCharacterActive == false) {
+      $("#main-character").empty();
+      $(this).detach().appendTo($("#main-character")).removeClass("float-left").addClass("float-right");
+      mainCharacterActive = true;
+      mainCharacterHP = parseInt($(this).attr("data-health-points"));
+      mainCharacterAP = parseInt($(this).attr("data-attack-power"));
+      mainFrenemieCAP = parseInt($(this).attr("data-counter-attack-power"));
+      $("#available-friends-title").text("Available Frenemies");
+      $("#available-characters-container").find(".character").removeClass("text-white").addClass("text-danger");
+    } else if (mainFrenemieActive == false) {
+      $("#current-frenemie").empty();
+      $(this).detach().appendTo($("#current-frenemie"));
+      mainFrenemieActive = true;
+      mainFrenemieHP = parseInt($(this).attr("data-health-points"));
+    }
+  });
+
+  //Activate the Button
+  $("#attack-button").on("click", function(){
+    if (mainCharacterActive == true && mainFrenemieActive == true) {
+
+    }
+  });
+
+
+
 
 
 
